7bde27a66a6e6762db16c2af66fd79eeac2bb551
Ricardo Solis <ricardo.solis@thomasgreg.com>
2020-01-29 16:51:17 +0000

    One2many relations added

--- openacademy/models/.ipynb_checkpoints/models-checkpoint.py
+++ openacademy/models/.ipynb_checkpoints/models-checkpoint.py
@@ -10,7 +10,9 @@ class Course(models.Model):
     description = fields.Text()
     responsible_id = fields.Many2one('res.users',
         ondelete='set null', string="Responsible", index=True)    
-
+    session_ids = fields.One2many(
+        'openacademy.session', 'course_id', string="Sessions")
+    
     
 class Session(models.Model):
     _name = 'openacademy.session'
