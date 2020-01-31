2938fec813e7496587338d384b79a46c74fb7b1e
Ricardo Solis <ricardo.solis@thomasgreg.com>
2020-01-31 20:43:33 +0000

    Menu elements structured in openacademy.xml v2

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -2,16 +2,21 @@
 
 from odoo import models, fields, api
 
+
 class Course(models.Model):
     _name = 'openacademy.course'
     _description = "OpenAcademy Courses"
 
-    name = fields.Char(string="Title", required=True)
+    name = fields.Char(string="Title", 
+                       required=True)
     description = fields.Text()
-    responsible_id = fields.Many2one('res.users',
-        ondelete='set null', string="Responsible", index=True)    
-    session_ids = fields.One2many(
-        'openacademy.session', 'course_id', string="Sessions")
+    responsible_id = fields.Many2one('res.users', 
+                                     ondelete='set null', 
+                                     string="Responsible", 
+                                     index=True)    
+    session_ids = fields.One2many('openacademy.session', 
+                                  'course_id', 
+                                  string="Sessions")
     
     
 class Session(models.Model):
@@ -20,24 +25,28 @@ class Session(models.Model):
 
     name = fields.Char(required=True)
     start_date = fields.Date()
-    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    duration = fields.Float(digits=(6, 2), 
+                            help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    instructor_id = fields.Many2one('res.partner', string="Instructor",
-        domain=['|', ('instructor', '=', True),
-                     ('category_id.name', 'ilike', "Teacher")])    
+    instructor_id = fields.Many2one('res.partner', 
+                                    string="Instructor", 
+                                    domain=['|', ('instructor', '=', True), 'category_id.name', 'ilike', "Teacher")])    
     course_id = fields.Many2one('openacademy.course',
-        ondelete='cascade', string="Course", required=True)
-    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+                                ondelete='cascade', 
+                                string="Course", 
+                                required=True)
+    attendee_ids = fields.Many2many('res.partner', 
+                                    string="Attendees")
 
 
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    # Add a new column to the res.partner model, by default partners are not
-    # instructors
-    instructor = fields.Boolean("Instructor", default=False)
-
+    # Add a new column to the res.partner model, by default partners are not instructors
+    instructor = fields.Boolean("Instructor", 
+                                default=False)
     session_ids = fields.Many2many('openacademy.session',
-        string="Attended Sessions", readonly=True)    
+                                   string="Attended Sessions", 
+                                   readonly=True)    
     
     
