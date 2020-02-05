4a8de4eb743797eb11b9c1bbb2a66290d1033711
Ricardo Solis <ricardo.solis@thomasgreg.com>
2020-02-05 14:38:04 +0000

    QWeb Report v2

--- openacademy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ openacademy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -21,13 +21,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'board'],
+    'depends': ['base'],
+    # 'depends': ['base', 'board'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/openacademy.xml',
-        'views/session_board.xml',
+        # 'views/session_board.xml',
         'reports.xml',
     ],
     # only loaded in demonstration mode
