1cdc1dda817f79d6dd59933e7a5eee576169ec80
Ricardo Solis <ricardo.solis@thomasgreg.com>
2020-02-05 14:27:59 +0000

    QWeb Report

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -21,12 +21,13 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/openacademy.xml',
+        'views/session_board.xml',
         'reports.xml',
     ],
     # only loaded in demonstration mode
